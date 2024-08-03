67cfd30ecc191fb053535f58e43a20e564c6296e
Miran Hamid <miranhamid2002@gmail.com>
2024-08-04 00:19:51 +0530

    update GET req message

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -1,8 +1,10 @@
-import express from 'express';
-import * as identityController from '../controllers/contactController.js';
+import express from "express";
+import * as identityController from "../controllers/contactController.js";
 
 const router = express.Router();
 
-router.post('/identify', identityController.identify);
-
+router.post("/identify", identityController.identify);
+router.get("/identify", (req, res) => {
+  res.send("Make a POST request, nothing here on GET");
+});
 export default router;
